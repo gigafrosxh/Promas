b048bcb8fde8bbe6091a1134d2bd8e3d851e4242
daniel <daniel.naderer@htlstp.at>
2025-10-09 22:06:25 +0200

    Initial Commit

---
+++ src/test/java/com/promaspioneers/promas/PromasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promaspioneers.promas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
